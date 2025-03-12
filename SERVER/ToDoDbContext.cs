4edbaceb0c8050b71812ba191bd4f1c9f5745f77
miriammo8467 <86227859+miriammo8467@users.noreply.github.com>
2025-03-12 02:00:07 +0200

    Your commit message

---
+++ SERVER/ToDoDbContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace TodoApi
+{
+    public partial class ToDoDbContext : DbContext
+    {
+        public ToDoDbContext()
+        {
+        }
+
+        public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Item> Items { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseMySql("server=localhost;user=root;password=dasi43420@;database=database", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.41-mysql"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasCharSet("utf8mb4")
+                .UseCollation("utf8mb4_0900_ai_ci");
+
+            modelBuilder.Entity<Item>(entity =>
+            {
+                entity.ToTable("items");
+
+                entity.Property(e => e.Name).HasMaxLength(100);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
