8f4c26678517b60511ddcd99fe72551121ef97a3
miriammo8467 <86227859+miriammo8467@users.noreply.github.com>
2025-03-18 14:57:12 +0200

    exelent

--- SERVER/ToDoDbContext.cs
+++ SERVER/ToDoDbContext.cs
@@ -24,7 +24,7 @@ namespace TodoApi
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseMySql("server=localhost;user=root;password=dasi43420@;database=database", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.41-mysql"));
+                optionsBuilder.UseMySql("server=bn2op2c8yg9wkqt7mcr6-mysql.services.clever-cloud.com;user=uq86fxkxmmq7h9fv;password=1VUyQz934DztdxakUuzY;database=bn2op2c8yg9wkqt7mcr6", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.41-mysql"));
             }
         }
 
